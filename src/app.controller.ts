af7d99731b2184497898441bfabd985a90d045b0
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-12-17 20:30:00 -0300

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        recipientId: randomUUID(),
+        content: 'Você está sendo notificado!',
+        category: 'social',
+      },
+    });
   }
 }
